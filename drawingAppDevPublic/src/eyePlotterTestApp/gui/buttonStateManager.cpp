018009dbd33456d61b6ac5f5ac3abbbb3105140b
Zach Lieberman <zach@zach-liebermans-macbook-pro-3.local>
2010-03-06 18:20:28 -0500

    removed unnecessary svn stuff.

--- drawingAppDevPublic/src/eyePlotterTestApp/gui/buttonStateManager.cpp
+++
@@ -1,9 +0,0 @@
-/*
- *  buttonStateManager.cpp
- *  openFrameworks
- *
- *  Created by theo on 17/08/2009.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
-
--- drawingAppDevPublic/src/eyePlotterTestApp/gui/timerButton.h
+++
@@ -1,9 +0,0 @@
-/*
- *  timerButton.h
- *  openFrameworks
- *
- *  Created by theo on 17/08/2009.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
-
